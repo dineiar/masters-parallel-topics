ed02268feea8fe1a498e51cdb45fdce2b2412b49
Dinei A. Rockenbach <dineiar@gmail.com>
2019-05-01 17:49:11 -0300

    Experiments

--- experiments/runall.sh
+++ experiments/runall.sh
@@ -2,15 +2,14 @@
 
 mvn -f ../ package
 
-MIN=1
-MAX=10
-EXPERIMENTS="CoarseListExperiment HoHListExperiment"
+EXPERIMENTS="CoarseListExperiment HoHListExperiment HoHContainsLockFreeListExperiment"
 
 for exp in $EXPERIMENTS;
 do
     OUTPUT="$exp.csv"
-    echo -n "Running $exp from $MIN to $MAX..."
-    for i in {$MIN..$MAX};
+    date >> $OUTPUT
+    echo -n "Running $exp from 1 to 10..."
+    for i in {1..10};
     do
         echo -n " $i"
         java -cp ../target/classes masters.experiment.$exp $i $i $i >> $OUTPUT
